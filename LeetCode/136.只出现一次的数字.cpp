4e21d8f7e80bdaff45e7c6f628df6367a8276b3f
njustbyq <2042675095@qq.com>
2021-11-30 21:25:03 +0800

    Single Numner

---
+++ LeetCode/136.只出现一次的数字.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=cpp
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+
+/*
+ *【遍历unordered_map】
+ * unordered_map<key, T>::iterator it; 
+ * (*it).first;     //the key value
+ * (*it).second;    //the mapped value
+ * for(unordered_map<key, T>::iterator iter=mp.begin();iter!=end();iter++)
+ * cout << "key value is" << iter->first << "the mapped value is" << iter->second
+ * 也可以这样:
+ * for(auto& v: mp)
+ *  print v.first and v.second
+ */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> ans;
+        for(int i = 0; i < nums.size(); i++) {
+            ans[nums[i]]++;
+        }
+        for(auto& n : ans) {
+            if(n.second == 1) {
+                return n.first;
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
