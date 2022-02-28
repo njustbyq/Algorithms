fcbdc43bb9b9ad954fb155c4ec427d1dfab9f5be
njustbyq <2042675095@qq.com>
2022-02-28 23:28:43 +0800

    InOrder Traversal of a BinaryTree

---
+++ LeetCode/94.二叉树的中序遍历.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/**
+ * 定义 inorder(root) 表示当前遍历到root节点的答案
+ * 按照定义,只要递归调用inorder(root->left)来遍历root节点的左子树,
+ * 然后将root节点的值加入答案, 再递归调用inorder(root->right)来遍历root节点的右子树即可
+ * 递归终止条件即为碰到空节点
+*/
+
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (!root) {
+            return;
+        }
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
+// @lc code=end
+
